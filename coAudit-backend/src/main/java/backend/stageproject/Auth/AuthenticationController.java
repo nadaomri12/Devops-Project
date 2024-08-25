5fcbc706f92bcb1ef3c1da63c45f207f6df3a94a
nadaomri12 <nadaomri2001@gmail.com>
2024-08-25 21:39:05 +0100

    fix jinkins file

--- coAudit-backend/src/main/java/backend/stageproject/Auth/AuthenticationController.java
+++ coAudit-backend/src/main/java/backend/stageproject/Auth/AuthenticationController.java
@@ -1,7 +1,5 @@
 package backend.stageproject.Auth;
 
-import backend.stageproject.Entity.Company;
-import backend.stageproject.Entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
