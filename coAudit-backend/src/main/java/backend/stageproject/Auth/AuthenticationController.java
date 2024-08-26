44b0a74f06496f344aa92f36d43338ac9d6ca44d
nadaomri12 <nadaomri2001@gmail.com>
2024-08-26 13:14:31 +0100

    fix jinkins file

--- coAudit-backend/src/main/java/backend/stageproject/Auth/AuthenticationController.java
+++ coAudit-backend/src/main/java/backend/stageproject/Auth/AuthenticationController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/auth")
-@CrossOrigin(origins = "http://localhost:4200")
+
 public class AuthenticationController {
     @Autowired
     private final AuthenticationService service;
--- coAudit-backend/src/main/java/backend/stageproject/configuration/CrossConfiguration.java
+++ coAudit-backend/src/main/java/backend/stageproject/configuration/CrossConfiguration.java
@@ -14,7 +14,7 @@ public class CrossConfiguration  {
                 @Override
                 public void addCorsMappings(CorsRegistry registry) {
                     registry.addMapping("/**")
-                            .allowedOrigins("*") // Autoriser toutes les origines
+                            .allowedOrigins("*") 
                             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Autoriser ces méthodes
                             .allowedHeaders("*") // Autoriser tous les en-têtes
                             .allowCredentials(true); // Autoriser les informations d'identification
